2714f1e28dafe7d76a9c2513af21757d37ecff77
rnogueira73 <ruben_nogueira@hotmail.com>
2024-10-17 12:01:36 -0600

    herencia y composicion II

--- Start/Ch 2/challenge.py
+++ Start/Ch 2/challenge.py
@@ -11,15 +11,35 @@
 # -- get_description returns formats for stocks and bonds
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
+from abc import ABC,abstractmethod
 
-class Asset():
-    pass
+class Asset(ABC):
+    def __init__(self,price):
+        self.price = price
 
-class Stock():
-    pass
+    @abstractmethod
+    def get_description(self):
+        pass
 
-class Bond():
-    pass
+class Stock(Asset):
+    def get_description(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+    
+    def __init__(self, ticker, price, company):
+        super().__init__(price)
+        self.ticker = ticker
+        self.company = company
+
+class Bond(Asset):
+    def get_description(self):
+        return f"{self.description}: {self.duration}yr : ${self.price} : {self.yieldamt}%"
+    
+    def __init__(self, price, description, duration, yieldamt):
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.yieldamt = yieldamt
+        
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
